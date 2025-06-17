436253cba06a0896071c16091e039f32bed79401
Jo�o Pedro Alves Belai <48914771806@>
2025-06-17 17:02:57 -0300

    exercicios prova comentados

--- Prova_Ex2.py
+++ Prova_Ex2.py
@@ -1,15 +1,19 @@
+# Classe Produto sendo instanciada
 class Produto:
+    # metedo de inicialização pedindo os atributos nome, quantidade disponível e preço unitário como obrigatórios
     def __init__(self, nome: str, qtd_disponivel: int, preco_unit: float):
         self.nome = nome
         self.qtd_disponivel = qtd_disponivel
         self.preco_unit = preco_unit
 
+    # método adicionar quantidade aumentando o self.qtd_disponivel
     def adcionar_quantidade(self, quantidade):
         print("\nAumento na quantidade de itens realizada com sucesso!")
         self.qtd_disponivel += quantidade
         print(f"Quantidade atual de {self.nome}: {self.qtd_disponivel}")
 
-
+    # método remover quantidade diminuindo o self.qtd_disponivel, desde que já não esteja zerada
+    # e caso diminua mais do que tem no estoque o valor do estoque é setado em 0
     def remover_quantidade(self, quantidade):
         if self.qtd_disponivel <=0:
             print("\nRemoção não pode ser realizada, você não tem mais itens desse tipo no estoque")
@@ -23,20 +27,24 @@ class Produto:
                 print("\nRemoção de itens realizada com sucesso!")
                 print(f"Quantidade atual de {self.nome}: {self.qtd_disponivel}")
     
+    # método calcular valor total do estoque que faz a quantidade de produtos vezs o preço unitario e da o valor total
     def calcular_valor_total(self):
         valor_total = self.qtd_disponivel * self.preco_unit
         print(f"\nValor total do estoque de {self.nome}: R${valor_total:.2f}")
 
 
 class KitProduto(Produto):
+    # método de inicialização puxando os valores da classe pai e criando a lista de produtos
     def __init__(self, nome, qtd_disponivel, preco_unit):
         super().__init__(nome, qtd_disponivel, preco_unit)
         self.lista_produto = []
 
+    # método adicionar produto que recebe um objeto Produto como parametro e adiciona ele na lista
     def adcionar_produto(self, produto: Produto):
         self.lista_produto.append(produto)
         print("\nProduto adicionado com sucesso!")
     
+    # método remover produto que recebe um objeto Produto como parametro e remove ele da lista caso ele esteja presente nela
     def remover_produto(self, produto: Produto):
         if produto in self.lista_produto:
             self.lista_produto.remove(produto)
@@ -44,6 +52,7 @@ class KitProduto(Produto):
         else:
             print("\nProduto não presente nesse kit")
     
+    # método calcular valor total do kit, para cada produto na lista ele multiplica a quantidade pelo preço e soma no valor do kit e passa o resultado
     def calcular_valor_total(self):
         self.preco_unit = 0
         for produto in self.lista_produto:
@@ -53,6 +62,7 @@ class KitProduto(Produto):
         print(f"\nValor total do kit: R${self.preco_unit:.2f}")
 
 
+#testes
 produto1 = Produto("Bomba de Combustível", 10, 55.9)
 produto2 = Produto("Vela", 15, 15.6)
 produto3 = Produto("Cabo de Vela", 15, 20.4)
--- Prova_Ex3.py
+++ Prova_Ex3.py
@@ -1,18 +1,22 @@
+# classe pai Veiculo que pede capacidade, placa, e custo como atributos obrigatórios
 class VeiculoTransporte:
     def __init__(self, capacidade: int, placa: str, custo_km: float):
         self.capacidade = capacidade
         self.placa = placa
         self.custo_km = custo_km
 
-    
+    # método calcular custo que recebe distância e a capacidade atual como parâmetro e retorna o valor da distância pelo custo do km
     def calcular_custo(self, distancia, capacidade_atual):
         total = distancia * self.custo_km
         print(f"\nO valor total da viagem foi: R${total:.2f}")
 
+# subclasse Onibus que herda Veículo
 class Onibus(VeiculoTransporte):
     def __init__(self, capacidade, placa, custo_km):
         super().__init__(capacidade, placa, custo_km)
     
+    # método calcular custo herdado do pai que retorna o valor da distância pelo custo do km
+    # mas caso o veículo esteja com capacidade superior a 80% ele tem um acréscimo de 15% no valor por km
     def calcular_custo(self, distancia, capacidade_atual):
         porcento = (capacidade_atual*100)/self.capacidade
         if porcento > 80:
@@ -22,10 +26,13 @@ class Onibus(VeiculoTransporte):
         total = distancia * self.custo_km
         print(f"\nO valor total da viagem foi: R${total:.2f}")
 
+# subclasse MicroOnibus que herda Veículo
 class MicroOnibus(VeiculoTransporte):
     def __init__(self, capacidade, placa, custo_km):
         super().__init__(capacidade, placa, custo_km)
-    
+
+    # método calcular custo herdado do pai que retorna o valor da distância pelo custo do km
+    # mas caso o veículo esteja com capacidade superior a 70% ele tem um acréscimo de 10% no valor por km
     def calcular_custo(self, distancia, capacidade_atual):
         porcento = (capacidade_atual*100)/self.capacidade
         if porcento > 70:
@@ -35,10 +42,13 @@ class MicroOnibus(VeiculoTransporte):
         total = distancia * self.custo_km
         print(f"\nO valor total da viagem foi: R${total:.2f}")
 
+# subclasse Van que herda Veículo
 class Van(VeiculoTransporte):
     def __init__(self, capacidade, placa, custo_km):
         super().__init__(capacidade, placa, custo_km)
-    
+
+    # método calcular custo herdado do pai que retorna o valor da distância pelo custo do km
+    # mas caso o veículo esteja com capacidade superior a 60% ele tem um acréscimo de 5% no valor por km
     def calcular_custo(self, distancia, capacidade_atual):
         porcento = (capacidade_atual*100)/self.capacidade
         if porcento > 60:
@@ -49,6 +59,7 @@ class Van(VeiculoTransporte):
         print(f"\nO valor total da viagem foi: R${total:.2f}")
 
 
+#testes
 onibus1 = Onibus(20, "ABC-1234", 2.0)
 micro_onibus1 = MicroOnibus(15, "ACB-1528", 3.0)
 van1 = Van(10, "BAC-3251", 3.5)
